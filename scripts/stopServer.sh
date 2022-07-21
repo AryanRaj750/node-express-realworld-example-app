8ae36931befbf0205bca990bbaea70f3464bc191
Aryan Raj <ar9131000@gmail.com>
2022-07-21 14:09:36 +0530

    Update stopServer.sh

--- scripts/stopServer.sh
+++ scripts/stopServer.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-sudo systemctl stop pm2-ubuntu
+pm2 describe app > /dev/null
+status=$?
+if [ $status -ne 0 ]
+then
+  echo 'App is not running'
+else
+  echo 'App is running'
+  echo 'App will be Terminated'
+  pm2 stop app
+fi
